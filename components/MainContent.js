7598aaf29c41fe055692270a604c9380e45568f1
bkraj0304 <raj0304.nit.m@gmail.com>
2024-08-08 01:32:49 +0530

    Changes in Image Tag

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,5 +1,6 @@
 // components/MainContent.js
 import React from 'react';
+import Image from 'next/image';
 import projectsData from '../data/projectsData';
 import tagsData from '../data/tagsData';
 
@@ -7,7 +8,7 @@ const MainContent = () => {
   return (
     <main>
       <section className="intro">
-        <p className="Greeting">Greetings! We're SnapNest, delving into travel,<br /> adventures, and exploration.</p>
+        <p className="Greeting">Greetings! We&apos;re SnapNest, delving into travel,<br /> adventures, and exploration.</p>
         <p className="Intro">Introducing a contemporary and polished template for showcasing your travel experiences. Ideal for presenting your trips, stories, and explorations in a professional and captivating manner...</p>
         <a href="#" className="member-button">BECOME A MEMBER</a>
       </section>
@@ -24,7 +25,12 @@ const MainContent = () => {
             <div className="project_row">
               {projectRow.map((project, projectIndex) => (
                 <div key={projectIndex} className="project-item">
-                  <img src={project.imgSrc} alt={project.alt} />
+                  <Image 
+                    src={project.imgSrc} 
+                    alt={project.alt} 
+                    width={500} 
+                    height={300} 
+                  />
                   <h3>{project.title}</h3>
                   <p>{project.category}</p>
                   <a href={project.link}>READ MORE</a>
@@ -47,7 +53,12 @@ const MainContent = () => {
             <div className="tag-row">
               {tagRow.map((tag, tagIndex) => (
                 <div key={tagIndex} className="tag-item">
-                  <img src={tag.imgSrc} alt={tag.alt} />
+                  <Image 
+                    src={tag.imgSrc} 
+                    alt={tag.alt} 
+                    width={150} 
+                    height={150} 
+                  />
                   <a href={tag.link}>{tag.name}</a>
                 </div>
               ))}
